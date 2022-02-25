267cd5884bbb0b41c7f28827cfbc6d4b9cb34319
lilstar <lilstar.shin@gmail.com>
2022-02-25 15:46:49 +0900

    View Controller Life Cycle Example

--- ScreenTransitionExample/ViewController.swift
+++ ScreenTransitionExample/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    print("ViewController 뷰가 로드 되었다.")
+  }
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    print("viewWillAppear 뷰가 나타날 것이다.")
+  }
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    print("viewDidAppear 뷰가 나타났다.")
+  }
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    print("viewWillDisappear 뷰가 사라질 것이다.")
+  }
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(animated)
+    print("viewDidDisappear 뷰가 사라졌다.")
+  }
+  
   @IBAction func tapCodePresentButton(_ sender: UIButton) {
     guard let viewController = self.storyboard?.instantiateViewController(identifier: "CodePresentViewController") else { return }
     viewController.modalPresentationStyle = .fullScreen
--- SuguePushViewController.swift
+++ SuguePushViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class SuguePushViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    print("Segue ViewController 뷰가 로드 되었다.")
+  }
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    print("Segue ViewWillAppear 뷰가 나타날 것이다.")
+  }
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    print("Segue ViewDidAppear 뷰가 나타났다.")
+  }
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    print("Segue ViewWillDisappear 뷰가 사라질 것이다.")
+  }
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(animated)
+    print("Segue ViewDidDisappear 뷰가 사라졌다.")
+  }
     
 
   @IBAction func tabBackButton(_ sender: UIButton) {
