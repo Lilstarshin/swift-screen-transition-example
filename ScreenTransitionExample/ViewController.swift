50f8df7596a1b20fae84a994e08561ea3ff0670e
lilstar <lilstar.shin@gmail.com>
2022-02-28 08:19:02 +0900

    Screen Transition Example

--- ScreenTransitionExample/ViewController.swift
+++ ScreenTransitionExample/ViewController.swift
@@ -7,7 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SendDataDelegate {
+  
+  @IBOutlet weak var nameLabel: UILabel!
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -31,14 +33,28 @@ class ViewController: UIViewController {
   }
   
   @IBAction func tapCodePresentButton(_ sender: UIButton) {
-    guard let viewController = self.storyboard?.instantiateViewController(identifier: "CodePresentViewController") else { return }
+    guard let viewController = self.storyboard?.instantiateViewController(identifier: "CodePresentViewController") as? CodePresentViewController else { return }
+    
     viewController.modalPresentationStyle = .fullScreen
+    viewController.name = "Gunter"
+    viewController.delegate = self
     self.present(viewController, animated: true, completion: nil)
   }
   @IBAction func tapCodePushButton(_ sender: UIButton) {
     
-    guard let viewController =  self.storyboard?.instantiateViewController(identifier: "CodePushViewController")  else { return }
+    guard let viewController =  self.storyboard?.instantiateViewController(identifier: "CodePushViewController") as? CodePushViewController  else { return }
+    viewController.name = "Gunter"
     self.navigationController?.pushViewController(viewController, animated: true)
   }
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if let viewController = segue.destination as? SuguePushViewController {
+      viewController.name = "Gunter"
+    }
+  }
+  func sendData(name: String) {
+    self.nameLabel.text = name
+    self.nameLabel.sizeToFit()
+  }
 }
+
 
